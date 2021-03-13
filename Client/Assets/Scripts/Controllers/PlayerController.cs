9c154755b052dad42f122876db7aa8eb57158d1f
BerryBless <shsuzakfn@gmail.com>
2021-03-13 20:28:00 +0900

    Day1) 플레이어 이동-1
    상하좌우 입력시 그방향으로 움직이게

--- Client/Assets/Scripts/Controllers/PlayerController.cs
+++ Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    float _speed = 6f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float axisV = Input.GetAxisRaw("Vertical");
+        float axisH = Input.GetAxisRaw("Horizontal");
+        Vector3 dir = Vector3.zero;
+        if (axisV != 0)
+        {
+            dir = axisV > 0 ? Vector3.up : Vector3.down;
+        }
+        else if (axisH != 0)
+        {
+            dir = axisH > 0 ? Vector3.right : Vector3.left;
+        }
+        else
+        {
+        }
+        transform.position += dir * Time.deltaTime * _speed;
     }
 }

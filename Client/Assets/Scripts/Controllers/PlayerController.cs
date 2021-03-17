fb1cadf3bf72a538948161fbcdc290855648a9e8
BerryBless <shsuzakfn@gmail.com>
2021-03-17 22:51:06 +0900

    Day4) State별로 업데이트하기
    버그 : 아이들 상태로 안바뀜

--- Client/Assets/Scripts/Controllers/PlayerController.cs
+++ Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -24,6 +24,7 @@ public class PlayerController : MonoBehaviour
         set
         {
             if (_state == value) return; _state = value;
+            UpdateAnimation();                  // 바뀔때 자동업데이트
         }
     }
     private MoveDir _dir = MoveDir.Down;       // 이동방향
@@ -33,14 +34,18 @@ public class PlayerController : MonoBehaviour
         set
         {
             if (_dir == value) return; _dir = value;
+            UpdateAnimation();                 // 바뀔떄 자동업데이트
         }
     }
-    bool _isMoving = false;// 판정좌표는 이동했으나, 스프라이트는 움직이는 중
     bool _isKeyPress = false;// 이동키 눌렀는지
     void Start()
     {
         //초기화
+        Init();
+    }
 
+    void Init()
+    {
         transform.position = _grid.CellToWorld(_cellPos) + _sprightCorrecrion;
         _sprite = GetComponent<SpriteRenderer>();
 
@@ -50,10 +55,62 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        PlayerUpdateController();
+    }
+    
+    private void LateUpdate()
+    {
+        // 카메라 따라가게
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+    }
+    void PlayerUpdateController()
+    {
+        switch (State)
+        {
+            case PlayerState.Idle:
+                UpdateIdle();
+                break;
+            case PlayerState.Moving:
+                UpdateMoving();
+                break;
+            case PlayerState.Attack:
+                UpdateAttack();
+                break;
+        }
+
+    }
+    // STATE가 IDLE일떄 업데이트 할것들
+    void UpdateIdle()
+    {
         GetDirInput();
-        UpdateIsMoveing();
-        UpdateMoving();
-        UpdateAnimation();
+        if(_isKeyPress == true)
+        {
+            State = PlayerState.Moving;
+        }
+    }
+
+    // STATE가 Move일때 업데이트 할것들
+    // 스르륵 움직이게!
+    void UpdateMoving()
+    {
+        Vector3 destPos = _grid.CellToWorld(_cellPos) + _sprightCorrecrion; // 목적지
+        Vector3 moveDir = destPos - transform.position;                     // 목적지까지의 방향백터
+
+        // 도착이냐
+        float dist = moveDir.magnitude;
+        if (dist < _speed * Time.deltaTime)
+        {
+            transform.position = destPos;
+            MoveToNextPos();
+        }
+        else
+        {
+            transform.position += moveDir.normalized * _speed * Time.deltaTime; // 정규화 방향으로 스피드만큼 이동
+        }
+    }
+    // STATE가 Attack일때 업데이트 할것들
+    void UpdateAttack() { 
+        // TODO 공격!
     }
 
     // 키보드 입력으로 방향 결정
@@ -72,66 +129,50 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            State = PlayerState.Idle;
             _isKeyPress = false;
         }
     }
 
     // 오브젝트 판정 실제로 이동
-    void UpdateIsMoveing()
+    void MoveToNextPos()
     {
-        if (_isKeyPress == true && _isMoving == false)
+        // 이동키 땠을때
+        if (_isKeyPress == false)
         {
-            switch (Dir)
-            {
-                case MoveDir.Up:
-                    _cellPos += Vector3Int.up;
-                    _isMoving = true; State = PlayerState.Moving;
-                    break;
-                case MoveDir.Down:
-                    _cellPos += Vector3Int.down;
-                    _isMoving = true; State = PlayerState.Moving;
-                    break;
-                case MoveDir.Left:
-                    _cellPos += Vector3Int.left;
-                    _isMoving = true; State = PlayerState.Moving;
-                    break;
-                case MoveDir.Right:
-                    _cellPos += Vector3Int.right;
-                    _isMoving = true; State = PlayerState.Moving;
-                    break;
-            }
-        }
-    }
-
-    // 유니티 스프라이트 이동
-    void UpdateMoving()
-    {
-        if (_isMoving == false) return;
-
-        Vector3 destPos = _grid.CellToWorld(_cellPos) + _sprightCorrecrion; // 목적지
-        Vector3 moveDir = destPos - transform.position;                     // 목적지까지의 방향백터
-
-        // 도착이냐
-        float dist = moveDir.magnitude;
-        if (dist < _speed * Time.deltaTime)
-        {
-            transform.position = destPos;
-            _isMoving = false;
             State = PlayerState.Idle;
+            return;
         }
-        else
+
+        Vector3Int destPos = _cellPos;
+        switch (Dir)
         {
-            transform.position += moveDir.normalized * _speed * Time.deltaTime; // 정규화 방향으로 스피드만큼 이동
-            _isMoving = true;
+            case MoveDir.Up:
+                destPos += Vector3Int.up;
+                break;
+            case MoveDir.Down:
+                destPos += Vector3Int.down;
+                break;
+            case MoveDir.Left:
+                destPos += Vector3Int.left;
+                break;
+            case MoveDir.Right:
+                destPos += Vector3Int.right;
+                break;
+        }
+
+        // 충돌체크
+        if (Managers.Map.CanGo(destPos) == true)
+        {
+            //TODO 오브젝트 충돌
+            _cellPos = destPos;
         }
     }
+
 
     void UpdateAnimation()
     {
         if (State == PlayerState.Idle)
         {
-            if (_isMoving == true || _isKeyPress == true) return;
             switch (_dir)
             {
                 case MoveDir.Up:
@@ -170,6 +211,28 @@ public class PlayerController : MonoBehaviour
                     break;
                 case MoveDir.Right:
                     _animator.Play("WALK_RIGHT");
+                    _sprite.flipX = false;
+                    break;
+            }
+        }
+        else if(State == PlayerState.Attack)
+        {
+            switch (_dir)
+            {
+                case MoveDir.Up:
+                    _animator.Play("ATTACK_BACK");
+                    _sprite.flipX = false;
+                    break;
+                case MoveDir.Down:
+                    _animator.Play("ATTACK_FRONT");
+                    _sprite.flipX = false;
+                    break;
+                case MoveDir.Left:
+                    _animator.Play("ATTACK_RIGHT");
+                    _sprite.flipX = true;
+                    break;
+                default:
+                    _animator.Play("ATTACK_RIGHT");
                     _sprite.flipX = false;
                     break;
             }

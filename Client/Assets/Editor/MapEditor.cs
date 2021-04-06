627fa44a9e08c36fdc662932a54c92fa6784f9ce
BerryBless <shsuzakfn@gmail.com>
2021-04-06 19:35:40 +0900

    Day11) MapEditor
    갈 수 없는 곳 처리만 하면 완성

--- Client/Assets/Editor/MapEditor.cs
+++ Client/Assets/Editor/MapEditor.cs
@@ -19,25 +19,38 @@ public class MapEditor : MonoBehaviour
         GenerateMap("../Common/MapData");       // 서버에서 쓸꺼
     }
 
-    private static List<List<int>> MakeAdj(ref int[,] map, int nodeCount, out int cnt)
+    private static int[,] MakeAdj(ref int[,] map, int nodeCount, int sizeY, int sizeX)
     {
-        List<List<int>> adj = new List<List<int>>();
-        // c++ 처럼 한번에 초기화 하는방법 없나?
-        for (int i = 0; i <= nodeCount; i++)
-            adj.Add(new List<int>());
-        cnt = 0;
-        // TODO 간선 만들기
+        int[,] adj = new int[nodeCount + 1, nodeCount + 1];
+        int[] dy = new int[] { 0, -1 };
+        int[] dx = new int[] { -1, 0 };
 
+        for (int i = 1; i <= nodeCount; i++)
+            for (int j = 1; j <= nodeCount; j++)
+            {
+                adj[i, j] = 0x3f3f3f3f;
+                if (i == j) adj[i, i] = 0;
+            }
+        for (int y = 0; y < sizeY; y++)
+        {
+            for (int x = 0; x < sizeX; x++)
+            {
+                if (map[y, x] == 0) continue;
+                for (int i = 0; i < 2; i++)
+                {
+                    if (y + dy[i] < 0 || x + dx[i] < 0 ||
+                        y + dy[i] >= sizeY || x + dx[i] >= sizeX) continue;
 
-        // TEMP 출력확인
-        //cnt = 7;
-        //adj[1].Add(3);
-        //adj[1].Add(2);
-        //adj[1].Add(4);
-        //adj[2].Add(5);
-        //adj[4].Add(7);
-        //adj[4].Add(5);
-        //adj[5].Add(6);
+                    if (map[y + dy[i], x + dx[i]] != 0 &&
+                        map[y + dy[i], x + dx[i]] != map[y, x])
+                    {
+                        // TODO Cost 책정
+                        adj[map[y + dy[i], x + dx[i]], map[y, x]] = 1;
+                        adj[map[y, x], map[y + dy[i], x + dx[i]]] = 1;
+                    }
+                }
+            }
+        }
 
         return adj;
     }
@@ -50,127 +63,125 @@ public class MapEditor : MonoBehaviour
 
         int[,] map;
         int nodeCount = 0;
-
+        int[,] adj;
+        int[,] floydPath;
 
         // 순회
         foreach (GameObject go in gameObjects)
         {
+            #region MakeMap
             Tilemap tmBase = go.FindChild<Tilemap>("Tilemap_Base", true);       // 맵전체 크기정보
             Tilemap tmColl = go.FindChild<Tilemap>("Tilemap_Collision", true);  // 충돌체크 정보가있는 콜리션
 
+            int sizeX = tmBase.cellBounds.xMax - tmBase.cellBounds.xMin + 1;
+            int sizeY = tmBase.cellBounds.yMax - tmBase.cellBounds.yMin + 1;
+
+            map = new int[sizeY, sizeX];
+
+            // 위에서 아래로
+            for (int y = tmBase.cellBounds.yMax; y >= tmBase.cellBounds.yMin; y--)
+            {
+                // 왼쪽에서 오른쪽으로
+                for (int x = tmBase.cellBounds.xMin; x <= tmBase.cellBounds.xMax; x++)
+                {
+                    // 추출해서
+                    TileBase tileColl = tmColl.GetTile(new Vector3Int(x, y, 0));
+
+                    if (tileColl != null)
+                    {
+                        Sprite t = tmColl.GetSprite(new Vector3Int(x, y, 0));
+                        int n;
+                        if (nodeNumber.TryGetValue(t.name, out n) == false)
+                        {
+                            nodeCount++;
+                            n = nodeCount;
+                            nodeNumber.Add(t.name, n);
+                        }
+                        map[tmBase.cellBounds.yMax - y, x - tmBase.cellBounds.xMin] = n;
+                    }
+                    // 없으면 0
+                    else
+                    {
+                        map[tmBase.cellBounds.yMax - y, x - tmBase.cellBounds.xMin] = 0;
+                    }
+                }
+            }
+            #endregion
+
+            #region MakePath
+            // 최단거리 캐싱
+            adj = MakeAdj(ref map, nodeCount, sizeY, sizeX);
+            floydPath = new int[nodeCount + 1, nodeCount + 1];
+
+            // memset 없나?
+            for (int i = 1; i <= nodeCount; i++)
+                for (int j = 1; j <= nodeCount; j++)
+                    floydPath[i, j] = -1;
+
+            // FLOYD algorithm
+            for (int k = 1; k <= nodeCount; k++)
+                for (int i = 1; i <= nodeCount; i++)
+                    for (int j = 1; j <= nodeCount; j++)
+                        if (adj[i, j] > adj[i, k] + adj[k, j])
+                        {
+                            floydPath[i, j] = k;
+                            adj[i, j] = adj[i, k] + adj[k, j];
+                        }
+            #endregion
+
+            #region Print
             // 추출한거 출력하기
             using (var writer = File.CreateText($"{pathPrefix}/{go.name}.txt"))
             {
-                #region MakeMap
+                #region PrintMap
                 writer.WriteLine(tmBase.cellBounds.xMin);
                 writer.WriteLine(tmBase.cellBounds.xMax);
                 writer.WriteLine(tmBase.cellBounds.yMin);
                 writer.WriteLine(tmBase.cellBounds.yMax);
 
-                int sizeX = tmBase.cellBounds.xMax - tmBase.cellBounds.xMin + 1;
-                int sizeY = tmBase.cellBounds.yMax - tmBase.cellBounds.yMin + 1;
-
-                map = new int[sizeY, sizeX];
-
                 // 위에서 아래로
-                for (int y = tmBase.cellBounds.yMax; y >= tmBase.cellBounds.yMin; y--)
+                for (int y = 0; y < sizeY; y++)
                 {
                     // 왼쪽에서 오른쪽으로
-                    for (int x = tmBase.cellBounds.xMin; x <= tmBase.cellBounds.xMax; x++)
+                    for (int x = 0; x < sizeX; x++)
                     {
-                        // 추출해서
-                        TileBase tileColl = tmColl.GetTile(new Vector3Int(x, y, 0));
-
-                        if (tileColl != null)
-                        {
-                            Sprite t = tmColl.GetSprite(new Vector3Int(x, y, 0));
-                            int n;
-                            if (nodeNumber.TryGetValue(t.name, out n) == false)
-                            {
-                                nodeCount++;
-                                n = nodeCount;
-                                nodeNumber.Add(t.name, n);
-                            }
-                            writer.Write($"{n} ");
-                            map[tmBase.cellBounds.yMax - y, x - tmBase.cellBounds.xMin] = n;
-                        }
-                        // 없으면 0
-                        else
-                        {
-                            writer.Write("0 ");
-                            map[tmBase.cellBounds.yMax - y, x - tmBase.cellBounds.xMin] = 0;
-                        }
+                        writer.Write($"{map[y, x]} ");
                     }
                     writer.WriteLine();
                 }
                 #endregion
 
-                #region MakePath
-                // 간선 생성
-                int adjCount;
-                List<List<int>> adj = MakeAdj(ref map, nodeCount, out adjCount);
-
-                //writer.WriteLine($"{nodeCount} {adjCount}");
-                //foreach (List<int> list in adj)
-                //    foreach (int i in list)
-                //        writer.WriteLine($"{adj.IndexOf(list)} {i}");
-
-                // 최단거리 캐싱
-                int[,] floyd = new int[nodeCount + 1, nodeCount + 1];
-                int[,] dp = new int[nodeCount + 1, nodeCount + 1];
-
-                for (int i = 1; i <= nodeCount; i++)
-                    for (int j = 1; j <= nodeCount; j++)
-                    {
-                        floyd[i, j] = 0x3f3f3f3f;
-                        dp[i, j] = -1;
-                        if (i == j) floyd[i, i] = 0;
-                    }
-                foreach (List<int> list in adj)
-                    foreach (int i in list)
-                    {
-                        // TODO 간선길이 주기
-                        floyd[i, adj.IndexOf(list)] = 1;
-                        floyd[adj.IndexOf(list), i] = 1;
-                    }
-
-                // FLOYD algorithm
-                for (int k = 1; k <= nodeCount; k++)
-                    for (int i = 1; i <= nodeCount; i++)
-                        for (int j = 1; j <= nodeCount; j++)
-                            if (floyd[i, j] > floyd[i, k] + floyd[k, j])
-                            {
-                                dp[i, j] = k;
-                                floyd[i, j] = floyd[i, k] + floyd[k, j];
-                            }
-                // 최단거리 출력
-                // TODO 갈수없는 곳(고립된곳) 처리
-                writer.WriteLine($"{nodeCount}");
-                List<int> path = new List<int>();
-                for (int i = 1; i <= nodeCount; i++)
+                #region PrintPath
                 {
-                    for (int j = 1; j <= nodeCount; j++)
+                    // 최단거리 출력
+                    // TODO 갈수없는 곳(고립된곳) 처리
+                    writer.WriteLine($"{nodeCount}");
+                    List<int> path = new List<int>();
+                    for (int i = 1; i <= nodeCount; i++)
                     {
-                        //writer.Write($"{i} {j} ");
-                        // path.size path.list[]
-                        if (i == j) { writer.WriteLine($"1 {j} "); continue; }
-                        path.Clear();
-                        path.Add(i); // here
-                        GetPath(i, j, ref path, ref dp);
-                        path.Add(j); // dest
-                        writer.Write($"{path.Count} ");
-                        foreach (int v in path)
+                        for (int j = 1; j <= nodeCount; j++)
                         {
-                            writer.Write($"{v} ");
-                        }
+                            //writer.Write($"{i} {j} ");
+                            // path.size path.list[]
+                            if (i == j) { writer.WriteLine($"1 {j} "); continue; }
+                            path.Clear();
+                            path.Add(i); // here
+                            GetPath(i, j, ref path, ref floydPath);
+                            path.Add(j); // dest
+                            writer.Write($"{path.Count} ");
+                            foreach (int v in path)
+                            {
+                                writer.Write($"{v} ");
+                            }
 
-                        writer.WriteLine();
+                            writer.WriteLine();
+                        }
                     }
                 }
 
-
                 #endregion
             }
+            #endregion
         }
     }
 
